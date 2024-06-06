ab70d65e5e417d308804c03b9d9bf3125e7df234
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2024-06-06 14:49:48 +0530

    fix: image

--- components/Footerimg.tsx
+++ components/Footerimg.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <>
             {/* <Header label="Home" /> */}
             <div className='mt-6 flex justify-center align-center'>
-            <Image src="/better.png" alt="Product screenshot" className="w-[72rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 " width="2432" height="1442"/>
+            <Image src="/betterbridgeshome.png" alt="Product screenshot" className="w-[72rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 " width="2432" height="1442"/>
             </div>
         </>
     )
